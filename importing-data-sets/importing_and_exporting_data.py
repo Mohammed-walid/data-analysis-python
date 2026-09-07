83ebb39262ebddbb5d0239896c9e5edf7d316e6e
Mohamed <mohamedwalid.elkholy@gmail.com>
2026-09-07 21:49:52 +0300

    getting started with pandas

--- importing-data-sets/importing_and_exporting_data.py
+++ importing-data-sets/importing_and_exporting_data.py
@@ -1,1 +1,5 @@
 """working with data module"""
+import pandas as pd
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+df = pd.read_csv(url, header = None)
+df.to_csv("automobile_dataset.csv", index=False)
