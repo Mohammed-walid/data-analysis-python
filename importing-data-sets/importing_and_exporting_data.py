aadd109edf678ae84995ee55582bbd061d497a3d
Mohamed <mohamedwalid.elkholy@gmail.com>
2026-09-07 21:56:42 +0300

    The csv file did not contain headers, so i made it

--- importing-data-sets/importing_and_exporting_data.py
+++ importing-data-sets/importing_and_exporting_data.py
@@ -2,4 +2,9 @@
 import pandas as pd
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(url, header = None)
-df.to_csv("automobile_dataset.csv", index=False)
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style",
+           "drive-wheels", "engine-location", "wheel-base", "length", "width", "height",
+           "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore",
+           "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"]
+df.columns = headers
+df.to_csv("automobile_dataset_with_headers.csv", index=False)
